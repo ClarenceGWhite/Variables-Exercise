b3c6a85d0334c2d20f2d81392f521583fa083f25
clarence <cgwhite76@gmail.com>
2022-10-14 14:59:09 -0500

    Final version of Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,6 +4,28 @@
     {
         static void Main(string[] args)
         {
+
+            string myDog;
+                myDog = "Gracie";
+
+            int myAge;
+                myAge = 68;
+
+            char myFirstLetter;
+                myFirstLetter = 'A';
+
+            bool myCollegeStatus;
+                myCollegeStatus = true;
+
+            double myStreetNumber;
+                myStreetNumber = 9950;
+
+            decimal myCurrentPocketChange;
+                myCurrentPocketChange = 1.50m;
+
+            Console.WriteLine($"My dog is named {myDog} and she lives at {myStreetNumber} Fountain Cir.");
+            Console.WriteLine($"I am {myAge} years old and I have ${myCurrentPocketChange} in my pocket.");
+            Console.WriteLine($"Am I essentially in college? {myCollegeStatus}!");
             Console.WriteLine("Hello, World!");
         }
     }
